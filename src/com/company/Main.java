f477d93bb9b0442a1502d176b42843a38d727236
Yana Wilkins <yav9774@gmail.com>
2017-08-15 15:23:06 -0400

    it works now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,56 +1,64 @@
 package com.company;
 
-class MarbleBag {
-       int red;
-       int green;
-       int blue;
+class MarbleBag
+{
+    // START
+
+    int red;
+    int green;
+    public int numBlueMarbles;
 
 
-//constructors for MarbleBag
-    public MarbleBag (Integer red, Integer green, Integer blue){
+    // constructors for MarbleBag
+    public MarbleBag (Integer red, Integer green, Integer blue) {
         this.red = red;
         this.green = green;
-        this.blue = blue;
+        this.numBlueMarbles = blue;
     }
 
-    //Method
-    public Integer getTotalNumberOfMarbles(){
-        return this.red + this.green + this.blue;
+    public Integer getTotalNumberOfMarbles() {
+        return red + green + numBlueMarbles;
     }
-      class RunnerMarbleBag{
 
-        //goes in the Runner from the assignment
-        public void main(String[] args) {
-            MarbleBag marbleBag = new MarbleBag(3, 1, 2);
+    public void printInfo() {
+        System.out.println(getTotalNumberOfMarbles());
+    }
 
-            ///STUCK Right here. Message: " Non-static mathod can't be
-            // referenced from static content"
-
-            marbleBag.printInfo(){
-                System.out.println(MarbleBag.getTotalNumberOfMarbles());
-            } ///                               ^ underlined in red with the above message
+    public void removeAllGreenMarbles() {
+        this.green = 0;
+    }
 
 
-            //Not even worried about this. for now...
-            marbleBag.numBlueMarbles = 5;
+    // END
+}
 
-            if (marbleBag.getTotalNumberOfMarbles() == 9) {
-                System.out.println("Total is 9, as expected");
-            } else {
-                System.out.println("PANIC!!!");
-                throw new AssertionError(); //Crashes the program
-            }
+class Main
+{
 
-            marbleBag.removeAllGreenMarbles();
+    // goes in the Runner from the assignment
+    public static void main(String[] args) {
+        MarbleBag marbleBag = new MarbleBag(3, 1, 2);
 
-            if (marbleBag.getTotalNumberOfMarbles() == 8) {
-                System.out.println("Total is 8, as expected");
-            } else {
-                System.out.println("Panic even more!!!");
-                throw new AssertionError();
-            }
+        marbleBag.printInfo();
 
-            }
+        marbleBag.numBlueMarbles = 5;
+
+        if (marbleBag.getTotalNumberOfMarbles() == 9) {
+            System.out.println("Total is 9, as expected");
+        } else {
+            System.out.println("PANIC!!!");
+            throw new AssertionError(); //Crashes the program
         }
+
+        marbleBag.removeAllGreenMarbles();
+
+        if (marbleBag.getTotalNumberOfMarbles() == 8) {
+            System.out.println("Total is 8, as expected");
+        } else {
+            System.out.println("Panic even more!!!");
+            throw new AssertionError();
+        }
+
     }
+}
 
